59c4d3b3cbb6376600ac810133b28a6c98de014b
Zurab Gignadze <z_gignadze@cu.edu.ge>
2026-03-03 18:24:49 +0400

    Week 1: FastAPI skeleton and basic UI upload page

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI(title="Retail Order Automation System")
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/ui/upload", response_class=HTMLResponse)
+def upload_page(request: Request):
+    return templates.TemplateResponse("upload.html", {"request": request})
